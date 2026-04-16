65f8e0943d667e825c579dc830c72cc36578a005
Miroslav Petrov <miroslavpetrov.link@gmail.com>
2026-04-16 15:03:17 +0300

    fix: tmdb proxy

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -1,47 +1,26 @@
-import { createProxyMiddleware } from "http-proxy-middleware";
 import { CLIENT_SIDE_BASE_URL } from "#shared/utils/api/constants";
+import { appAxiosInstance } from "#shared/utils/api/api";
 
-const target = process.env.NUXT_BASE_URL;
-const V4_API_KEY = process.env.NUXT_V4_API_KEY;
+export default defineEventHandler(async (event) => {
+  try {
+    const method = event.node.req.method || "GET";
+    const url = event.path.replace(new RegExp(`^${CLIENT_SIDE_BASE_URL}`), "");
 
-const apiProxyMiddleware = createProxyMiddleware({
-  pathRewrite: { [`^${CLIENT_SIDE_BASE_URL}`]: "" },
-  target,
-  changeOrigin: true,
-  secure: false,
-  headers: {
-    Authorization: `Bearer ${V4_API_KEY}`,
-  },
-  logger: process.env["NODE_ENV"] === "development" ? console : undefined,
-  on: {
-    error: (_err, _req, res) => {
-      if ("writeHead" in res) {
-        res.writeHead(500, {
-          "Content-Type": "text/plain",
-        });
-        res.end("Something went wrong.");
-      }
-    },
-  },
+    let body = undefined;
+    if (method !== "GET" && method !== "HEAD") {
+      body = await readBody(event);
+    }
+
+    const response = await appAxiosInstance.request({
+      url,
+      method,
+      data: body,
+    });
+
+    return response.data;
+  } catch (error) {
+    setResponseStatus(event, 500);
+    setResponseHeader(event, "Content-Type", "text/plain");
+    return "Something went wrong.";
+  }
 });
-
-export default defineEventHandler(
-  async (event) =>
-    new Promise(async (resolve, reject) => {
-      const next = (err?: Error) => {
-        event.node.res.off("close", next);
-        event.node.res.off("error", next);
-
-        return err ? reject(createError(err)) : resolve(undefined);
-      };
-
-      try {
-        event.node.res.once("close", next);
-        event.node.res.once("error", next);
-
-        return apiProxyMiddleware(event.node.req, event.node.res, next);
-      } catch (error) {
-        next(error as Error);
-      }
-    }),
-);
